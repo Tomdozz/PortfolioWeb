0bc27581cf26dddaa3ff4d69594ffc27da327efa
Tomdozz <tom_1995@msn.com>
2018-04-19 21:55:40 +0200

    no progress, too sleepy

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var height = $(document).height();
+console.log($(document).height());
+
 function displayMenu() {
-    
+
+}
+function pageCount() {
+    var height = $(document).height();
+    console.log(height);
 }
